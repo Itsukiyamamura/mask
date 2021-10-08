beb84640bfb5904a8e58ed0bf03f38e2aaf9689b
Y.I <66396019+Itsukiyamamura@users.noreply.github.com>
2021-10-08 13:12:35 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+// Window幅に応じたメニュー表示の変更
+function ShowAndHide(win){
+  if(win > 640){
+    $("#button").hide();
+    $("#lists").show();
+  }else{
+    $("#button").show();
+    $("#lists").hide();
+  }
+}
+
+$( function(){
+
+  // 初めてページを開いた時の状態チェック
+  var win = $(window).width();
+  ShowAndHide(win);
+
+  // Windowサイズが変更された時の状態チェック
+  $(window).resize(function(){
+    var win = $(window).width();
+    ShowAndHide(win);
+  });
+
+  // MENUボタンクリック時のトグル動作
+  $("#button").click( function () {
+    $("#lists").slideToggle();
+  });
+
+});
